744f494aaf6dd10a317620f6430f0a35410f8e3c
Larry Kennan <165167990+LarryKennan@users.noreply.github.com>
2024-06-13 23:16:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 msg = "Hello, World!"
 print(msg[2:5])
+1msg = "Hello, World!"
+2msg = "Hello, World!"
+3msg = "Hello, World!"
+4msg = "Hello, World!"
+5msg = "Hello, World!"
+6msg = "Hello, World!"
